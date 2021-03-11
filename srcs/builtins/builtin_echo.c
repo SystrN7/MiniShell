4ed9b133ed23c018f29375bc9582279248a60f49
Felix Galaup <felixgalaup@yahoo.fr>
2021-03-11 11:16:13 +0100

    [^]-[Builtin](Echo)-Fix option n.

--- srcs/builtins/builtin_echo.c
+++ srcs/builtins/builtin_echo.c
@@ -6,21 +6,19 @@
 /*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 14:00:30 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/02/08 15:35:53 by fgalaup          ###   ########lyon.fr   */
+/*   Updated: 2021/03/10 14:45:13 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_builtin.h"
 
-int		builtin_echo(t_shell_context *context, int argv, char **args)
+int	builtin_echo(t_shell_context *context, int argc, char **args)
 {
 	int		i;
 	t_bool	option_n;
 
-	if (ft_strncmp("-n", args[0], 2))
-		option_n = TRUE;
-	else
-		option_n = FALSE;
+	if (argc > 1)
+		option_n = builtin_echo_option_n(args);
 	i = option_n;
 	while (args[i])
 	{
@@ -31,4 +29,27 @@ int		builtin_echo(t_shell_context *context, int argv, char **args)
 	if (option_n)
 		write(1, "\n", 1);
 	return (0);
+}
+
+int	builtin_echo_option_n(char **args)
+{
+	int		i;
+	t_bool	option_n;
+
+	i = 0;
+	option_n = 0;
+	if (args[1][i] == '-')
+	{
+		while (args[1][i])
+		{
+			if (args[1][i++] == 'n')
+				option_n = TRUE;
+			else
+			{
+				option_n = FALSE;
+				break ;
+			}
+		}
+	}
+	return (option_n);
 }
