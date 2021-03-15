16f1845730de1751c62d1977c8fa25f9b49e0631
Felix Galaup <felixgalaup@yahoo.fr>
2021-03-15 10:37:24 +0100

    [^]-[Builtin](Exit)-Fix error order.

--- srcs/builtins/builtin_exit.c
+++ srcs/builtins/builtin_exit.c
@@ -6,34 +6,33 @@
 /*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 13:28:33 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/03/11 14:10:01 by fgalaup          ###   ########lyon.fr   */
+/*   Updated: 2021/03/15 10:17:11 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_builtin.h"
 
-/*
-** Exit shell and return given status code.
-**  - Withous params is return last command status code
-**  - exit is write on stderr in error case
-**  - The arg need to be numeric status code 255
-** 	(bash: exit: 9995afds5: numeric argument required)
-**  - If more than one argument is given
-**    is cause error and shell dosent exit (bash: exit: too many arguments)
-*/
+/**
+ * Exit shell and return given status code.
+ *  - Withous params is return last command status code
+ *  - exit is write on stderr in error case
+ *  - The arg need to be numeric status code 255
+ * 	(bash: exit: 9995afds5: numeric argument required)
+ *  - If more than one argument is given
+ *    is cause error and shell dosent exit (bash: exit: too many arguments)
+ **/
 
 int	builtin_exit(t_shell_context *context, int argc, char **args)
 {
 	ft_printf_fd(standard_error, "exit\n");
-	if (argc == 2)
+	if (argc >= 2)
 	{
 		if (!ft_strtest(args[arg1], ft_isdigit))
 			exit_error_invalid_argument(context, args[arg1]);
+		else if (argc > 2)
+			return (exit_error_too_many_argument(context));
 		context->last_command_return_code = ft_atoi(args[arg1]);
-		shell_shutdown(context);
 	}
-	else if (argc > 2)
-		return (exit_error_too_many_argument(context));
 	shell_shutdown(context);
 	return (0);
 }
