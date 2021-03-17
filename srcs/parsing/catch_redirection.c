25a428d9c36ca8c4ef5ca6e85d9dfdc52e09b682
seruiz <seruiz@student.le-101.fr>
2021-03-17 16:17:31 +0100

    backup

--- srcs/parsing/catch_redirection.c
+++ srcs/parsing/catch_redirection.c
@@ -6,7 +6,7 @@
 /*   By: seruiz <seruiz@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 12:53:04 by seruiz            #+#    #+#             */
-/*   Updated: 2021/03/17 15:07:28 by seruiz           ###   ########lyon.fr   */
+/*   Updated: 2021/03/17 15:36:06 by seruiz           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 #include <unistd.h>
 
-int	ft_redirection_found(t_shell_command *cmd, int i)
+int	ft_redirection_left(t_shell_command *cmd, int i)
 {
 	int	j;
 
@@ -26,16 +26,36 @@ int	ft_redirection_found(t_shell_command *cmd, int i)
 	return (j);
 }
 
+int	ft_redirection_right(t_shell_command *cmd, int i)
+{
+	int	j;
+
+	(void)cmd;
+	(void)i;
+	j = 0;
+
+	while (cmd->command_string[i] == ' ')
+		i++;
+	j = i;
+	return (j);
+}
+
 void	ft_catch_redirection(t_shell_context *context, t_shell_command *cmd)
 {
 	int	i;
+
+	//Split pendant la recherche de redirections?
 
 	(void)context;
 	i = 0;
 	while (cmd->command_string[i])
 	{
-		if ((cmd->command_string[i] == '>' || cmd->command_string[i] == '<') && cmd->command_mask[i] == '0')
-			i = ft_redirection_found(cmd, i);
+		if (cmd->command_string[i] == '\\')
+			i++;
+		else if (cmd->command_string[i] == '>' && cmd->command_mask[i] == '0')
+			i = ft_redirection_right(cmd, i);
+		else if (cmd->command_string[i] == '<' && cmd->command_mask[i] == '0')
+			i = ft_redirection_left(cmd, i);
 		else
 			i++;
 	}
