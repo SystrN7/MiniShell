7bb64a75377f814a3244f6adfc4fc0c9df54a4b7
seruiz <seruiz@student.le-101.fr>
2021-03-17 17:06:48 +0100

    last of the day

--- srcs/minishell_type.h
+++ srcs/minishell_type.h
@@ -6,7 +6,7 @@
 /*   By: seruiz <seruiz@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 16:19:16 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/03/17 15:11:03 by seruiz           ###   ########lyon.fr   */
+/*   Updated: 2021/03/17 16:44:17 by seruiz           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,13 @@ enum e_shell_args
 	arg3,
 };
 
+typedef struct	s_argv_list
+{
+	char				*argv;
+	struct s_argv_list	*next;
+}				t_argv_list;
+
+
 typedef struct	s_redirection_list
 {
 	char						redirection_type;
@@ -74,6 +81,7 @@ typedef struct s_shell_command
 	char				*command_mask;
 	char				*path;
 	char				**argv;
+	t_argv_list			*argv_lst;
 	t_redirection_list	*redirection;
 }				t_shell_command;
 
--- srcs/parsing/catch_redirection.c
+++ srcs/parsing/catch_redirection.c
@@ -6,7 +6,7 @@
 /*   By: seruiz <seruiz@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 12:53:04 by seruiz            #+#    #+#             */
-/*   Updated: 2021/03/17 15:36:06 by seruiz           ###   ########lyon.fr   */
+/*   Updated: 2021/03/17 17:06:33 by seruiz           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,30 @@
 #include "minishell_utilities.h"
 
 #include <unistd.h>
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list *lst;
+
+	if (alst == NULL || new == NULL)
+		return ;
+	if (*alst != NULL)
+	{
+		lst = ft_lstlast(*alst);
+		lst->next = new;
+	}
+	else
+		*alst = new;
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
 
 int	ft_redirection_left(t_shell_command *cmd, int i)
 {
@@ -29,7 +53,9 @@ int	ft_redirection_left(t_shell_command *cmd, int i)
 int	ft_redirection_right(t_shell_command *cmd, int i)
 {
 	int	j;
+	t_redirection_list	*new;
 
+	new = malloc(sizeof(t_redirection_list));
 	(void)cmd;
 	(void)i;
 	j = 0;
@@ -37,7 +63,21 @@ int	ft_redirection_right(t_shell_command *cmd, int i)
 	while (cmd->command_string[i] == ' ')
 		i++;
 	j = i;
+	while (ft_isalnum(cmd->command_string[j]) == 1)
+		j++;
+	new->redirection_file = malloc(sizeof(char) * (j - i));
+	new->redirection_file[j - i] = '\0';
+	ft_lstadd_back(&cmd->redirection, new);
 	return (j);
+}
+
+int	ft_argv_list(t_shell_command *cmd, int i)
+{
+	int j;
+
+	j = 0;
+	(void)cmd;
+	(void)i;
 }
 
 void	ft_catch_redirection(t_shell_context *context, t_shell_command *cmd)
@@ -48,15 +88,14 @@ void	ft_catch_redirection(t_shell_context *context, t_shell_command *cmd)
 
 	(void)context;
 	i = 0;
+
 	while (cmd->command_string[i])
 	{
-		if (cmd->command_string[i] == '\\')
-			i++;
-		else if (cmd->command_string[i] == '>' && cmd->command_mask[i] == '0')
+		if (cmd->command_string[i] == '>' && cmd->command_mask[i] == '0' && cmd->command_string[i - 1] != '\\')
 			i = ft_redirection_right(cmd, i);
-		else if (cmd->command_string[i] == '<' && cmd->command_mask[i] == '0')
+		else if (cmd->command_string[i] == '<' && cmd->command_mask[i] == '0' && cmd->command_string[i - 1] != '\\')
 			i = ft_redirection_left(cmd, i);
 		else
-			i++;
+			i = ft_argv_list(cmd, i);
 	}
 }
