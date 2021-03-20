c3e7b9440936a19a6fb59040d65fd1b941331ad2
seruiz <seruiz@student.le-101.fr>
2021-03-20 10:24:07 +0100

    update types

--- srcs/minishell_type.h
+++ srcs/minishell_type.h
@@ -6,7 +6,7 @@
 /*   By: seruiz <seruiz@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 16:19:16 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/03/19 11:45:54 by seruiz           ###   ########lyon.fr   */
+/*   Updated: 2021/03/19 15:30:25 by seruiz           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,13 +60,6 @@ enum e_shell_args
 	arg3,
 };
 
-typedef struct	s_argv_list
-{
-	char				*argv;
-	struct s_argv_list	*next;
-}				t_argv_list;
-
-
 typedef struct	s_redirection_list
 {
 	char						redirection_type;
@@ -81,7 +74,6 @@ typedef struct s_shell_command
 	char				*command_mask;
 	char				*path;
 	char				**argv;
-	t_argv_list			*argv_lst;
 	t_redirection_list	**redirection;
 }				t_shell_command;
 
