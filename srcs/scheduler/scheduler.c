62b4b88c20b897d86e45c6966561c3a0204d1bb5
Felix Galaup <felixgalaup@yahoo.fr>
2021-04-04 16:34:24 +0200

    [+]-[Minishell](Scheduler)-Little fix + shorter code.

--- srcs/scheduler/scheduler.c
+++ srcs/scheduler/scheduler.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 13:11:11 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/04/04 13:13:01 by fgalaup          ###   ########lyon.fr   */
+/*   Updated: 2021/04/04 14:53:31 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ t_node_binary	*scheduler(t_shell_context *context, t_node_binary *unschedule_roo
 	if (consistency_analyzer(context, unschedule_root))
 		return (NULL);
 	schedule_root = &unschedule_root;
+	binnary_show(*schedule_root, 4, "Root");
+	printf("\n");
 	schedule(schedule_root, *schedule_root);
-	// schedule_separator(schedule_root, *schedule_root);
-	// schedule_or_and(schedule_root, *schedule_root);
-	binnary_show(*schedule_root, 4, "root");
+	binnary_show(*schedule_root, 4, "Root");
 	printf("\n");
 	show_command(*schedule_root, 0);
 	printf("\n");
@@ -46,12 +46,9 @@ int	schedule(
 {
 	int		recution_state;
 
-	printf("Plop-\n");
 	recution_state = schedule_separator(current_root, previous_node);
-	printf("TEST-%d\n", recution_state);
 	if (recution_state == IS_SORT)
 	{
-		printf("TEST-TEST\n");
 		recution_state = schedule_or_and(current_root, previous_node);
 		if (recution_state == IS_SORT)
 			return (SORTED);
@@ -66,9 +63,7 @@ int	schedule_separator(
 {
 	t_node_binary	*current_node;
 	t_node_binary	**working_root;
-	t_bool			is_junction;
 
-	is_junction = FALSE;
 	working_root = current_root;
 	while (get_node_type(previous_node) == SHELL_SEPARATOR_TYPE_END)
 	{
@@ -83,17 +78,12 @@ int	schedule_separator(
 		if (get_node_type(current_node) == SHELL_SEPARATOR_TYPE_END)
 		{
 			schedule_swap(working_root, current_node, previous_node);
-			is_junction = TRUE;
-			break ;
+			schedule(&current_node->left, current_node);
+			schedule(&current_node->right, current_node);
+			return (SORTED);
 		}
 		previous_node = current_node;
 		current_node = current_node->left;
-	}
-	if (is_junction)
-	{
-		schedule(&current_node->left, current_node);
-		schedule(&current_node->right, current_node);
-		return (SORTED);
 	}
 	return (IS_SORT);
 }
@@ -107,20 +97,27 @@ int	schedule_or_and(
 	t_node_binary	**working_root;
 
 	working_root = current_root;
-	while (get_node_type(previous_node) == SHELL_SEPARATOR_TYPE_OR
-		|| get_node_type(previous_node) == SHELL_SEPARATOR_TYPE_AND)
+	current_node = previous_node;
+	while (get_node_type(current_node) == SHELL_SEPARATOR_TYPE_OR
+		|| get_node_type(current_node) == SHELL_SEPARATOR_TYPE_AND)
 	{
-		working_root = &previous_node->left;
-		previous_node = previous_node->left;
+		ft_printf("ke le pls \n");
+		working_root = &current_node->left;
+		previous_node = current_node;
+		current_node = current_node->left;
 	}
-	current_node = previous_node->left;
-	// current_node = previous_node;
+	ft_printf("befor swap :\n - previous node = %s\n - current node =%s\n - current root=%s\n\n",
+		node_get_value(previous_node),
+		node_get_value(previous_node->left),
+		node_get_value(*current_root)
+	);
 	while (current_node != NULL)
 	{
 		if (get_node_type(current_node) == SHELL_INSTRUCTION_COMMAND)
 			return (IS_SORT);
-		if (get_node_type(current_node) == SHELL_SEPARATOR_TYPE_OR
-			|| get_node_type(previous_node) == SHELL_SEPARATOR_TYPE_AND)
+		if ((get_node_type(current_node) == SHELL_SEPARATOR_TYPE_OR
+				|| get_node_type(current_node) == SHELL_SEPARATOR_TYPE_AND)
+			&& get_node_type(previous_node) != SHELL_SEPARATOR_TYPE_END)
 		{
 			schedule_swap(working_root, current_node, previous_node);
 			schedule(&current_node->left, current_node);
