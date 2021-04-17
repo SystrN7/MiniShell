a83174cbfd8d6990ea04e9e4a909d20fe68dc2e5
Felix Galaup <felixgalaup@yahoo.fr>
2021-04-17 15:25:19 +0200

    [^]-[Minishell](Prompt)-norm.

--- srcs/prompt/prompt.c
+++ srcs/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 12:39:41 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/04/15 16:01:06 by fgalaup          ###   ########lyon.fr   */
+/*   Updated: 2021/04/17 14:59:47 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	prompt(t_shell_context *context, const char *argv[])
 {
 	int		prompt_type;
-	
+
 	prompt_type = identify_prompt_type(argv);
 	if (prompt_type == PROMPT_TYPE_COMMAND)
 		return (prompt_command(context, argv[arg2]));
@@ -38,20 +38,3 @@ int	identify_prompt_type(const char *argv[])
 		return (PROMPT_TYPE_TTY);
 	return (PROMPT_TYPE_FILE);
 }
-
-
-	// while (argv[i])
-	// {
-	// 	j = 0;
-	// 	if (argv[i][j++] == '-')
-	// 	{
-	// 		while (argv[i][j])
-	// 		{
-	// 			if (argv[i][j] == 'c')
-	// 				return (AR);
-	// 			else
-	// 				return ();
-	// 		}
-	// 	}
-	// 	i++;
-	// }
